b0614c51d02f9a1f429826050665d902a423634d
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-03-02 21:05:37 +0100

    Add files via upload

---
+++ Üditőautomata/Automata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Üditőautomata
+{
+    class Automata
+    {
+        public string nev;
+        public string tipus;
+        public double meret;
+        public int ar;
+        public int maxmennyiseg;
+        public int jelenlegimennyiseg;
+        public string kod;
+
+        public Automata(string sor)
+        {
+            var dbok = sor.Split(';');
+            this.nev = dbok[0];
+            this.tipus = dbok[1];
+            this.meret = double.Parse(dbok[2]);
+            this.ar = int.Parse(dbok[3]);
+            this.maxmennyiseg = int.Parse(dbok[4]);
+            this.jelenlegimennyiseg = int.Parse(dbok[5]);
+            this.kod = dbok[6];
+
+        }
+    }
+}
